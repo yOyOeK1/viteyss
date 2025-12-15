1b3f080b33ae4ed23af64921ce040079a4d92665
Bartlomiej Ceglik <yoyoek@wp.pl>
2025-12-15 15:39:46 -0500

    freez local 12

--- startItAs.js
+++ startItAs.js
@@ -13,109 +13,147 @@ const __dirnameProcess = process.cwd();
 
 
 
+let Viteyss = (
+    isAs = 'local',
+    instanceTitle = 'local:BigOne',
 
-let instanceTitle = 'local:BigOne';
+    hostPublicIp = 'localhost',
+    pathsToSites = -1,
+    doSSL = false, // need to have keys in `viteyss/cert/...`
+) => {
 
-process.title = instanceTitle;
-let pathNodeYss = path.join( nyss.telMeYourHome(`${instanceTitle} - startItAsDev.js`),instanceTitle );
+  
+  process.title = instanceTitle;
+  let pathNodeYss = path.join( nyss.telMeYourHome(`${instanceTitle} - startItAs.js`),instanceTitle );
 
-function cl(str){
-    console.log('staI',str);
-
-}
-
-
-var pathsToSites = [
-    // dev 
-    '/home/yoyo/Apps/oiyshTerminal/ySS_calibration/sites',
-
-    // node-yss / sites
-    //path.join( pathNodeYss, 'sites' ),
-
-    // viteyss / sites
-    path.resolve('./sites'),
-
-    // if you have `~/.viteyss/sites
-    path.join( process.env.HOME, '.viteyss', 'sites' ),
-];
-var pathToSitesPackages = [];
-
-// sites ass a plugins `viteyss-site-`
-if( 1 ){
-  pcNpmls();
-  console.log("---------------------",
-    "vysPlugins",vysPlugins
-  );
-  if( Object.keys( vysPlugins ).length > 0 ){
-    Object.keys( vysPlugins ).forEach((pkey)=>{
-      pathsToSites.push( vysPlugins[pkey].pathTo );
-      // add path to sites from packages 
-      let ptp = {
-        "package":vysPlugins[pkey].package,
-        "pathTo":vysPlugins[pkey].pathTo
-      };
-      pathToSitesPackages.push( ptp );
-    });
+  function cl(str){
+      console.log('staI',str);
   }
+
+
+
+  if( isAs == 'local' ){ 
+  }else  if( isAs == 'devLocal' ){
+   
+  }else  if( isAs == 'devOT' ){
+  }
+
+  let pathToYss = pathNodeYss;
+
+  if( isAs == 'local' ){ 
+      pathsToSites = [
+        // node-yss / sites
+        path.join( pathNodeYss, 'sites' ),
+
+        // viteyss / sites
+        path.resolve('./sites'),
+
+        // if you have `~/.viteyss/sites
+        path.join( process.env.HOME, '.viteyss', 'sites' ),
+      ];
+
+  }else  if( isAs == 'devLocal' ){ 
+      pathsToSites = [
+        // dev 
+        '/home/yoyo/Apps/oiyshTerminal/ySS_calibration/sites',
+
+        // node-yss / sites
+        //path.join( pathNodeYss, 'sites' ),
+
+        // viteyss / sites
+        path.resolve('./sites'),
+
+        // if you have `~/.viteyss/sites
+        path.join( process.env.HOME, '.viteyss', 'sites' ),
+      ];
+
+  }else  if( isAs == 'devOT' ){ 
+      hostPublicIp = '192.168.43.220';
+      doSSL = true; 
+      pathToYss = '/home/yoyo/Apps/oiyshTerminal/ySS_calibration';
+
+      pathsToSites = [
+        // dev 
+        path.join( pathToYss, 'sites' ),
+
+        // node-yss / sites
+        //path.join( pathNodeYss, 'sites' ),
+
+        // viteyss / sites
+        path.resolve('./sites'),
+
+        // if you have `~/.viteyss/sites
+        path.join( process.env.HOME, '.viteyss', 'sites' ),
+      ];
+  }
+
+
+  var pathToSitesPackages = [];
+
+  // sites ass a plugins `viteyss-site-`
+  if( 1 ){
+    pcNpmls();
+    console.log("---------------------",
+      "vysPlugins",vysPlugins
+    );
+    if( Object.keys( vysPlugins ).length > 0 ){
+      Object.keys( vysPlugins ).forEach((pkey)=>{
+        pathsToSites.push( vysPlugins[pkey].pathTo );
+        // add path to sites from packages 
+        let ptp = {
+          "package":vysPlugins[pkey].package,
+          "pathTo":vysPlugins[pkey].pathTo
+        };
+        pathToSitesPackages.push( ptp );
+      });
+    }
+  }
+
+
+  
+  var config0 = {
+    'https': doSSL,
+    'name': instanceTitle,
+    'HOST': '0.0.0.0',
+    'PORT': 8080,
+    'wsHOST': '0.0.0.0',
+    'wsPORT': 2999,
+    'pathToYss': pathToYss,//'/home/yoyo/Apps/oiyshTerminal/ySS_calibration',
+    'pathsToSites': pathsToSites,
+    "pathsToSitesPackages": pathToSitesPackages,
+    //'wsInjection': false,
+    'wsInjection': true,
+    
+    'sitesInjection': true,
+    'ws': undefined,
+    'wsPinger': false
+  };
+
+  if( config0.https == true ){
+    config0['yssWSUrl'] = `wss://${hostPublicIp}:${config0.PORT}/fooWSS`;
+  } else {
+    // old ws way
+    config0['yssWSUrl'] = `ws://${hostPublicIp}:${config0.wsPORT}`;
+    
+    // noService ws as websocket as plugin in vite
+    //config0['yssWSUrl'] = `ws://${hostPublicIp}:${config0.PORT}/fooWSS`;
+
+  }
+
+
+  cl(["\n\n---------------------------\nHello - As Dev process name ["+instanceTitle+"]\n",
+    " - dirname: ",__dirname,
+    "\n -------------------------------------------\n\n"
+  ]);
+
+  let sc0 = new serverContainerVite(0,config0 );
+  sc0.initServers();
+  sc0.startServer();
+
+
+  cl("Done --- end");
+
+
 }
 
-
-var hostPublicIp = '192.168.43.220';
-var config0 = {
-  'https': true,
-  'name': instanceTitle,
-  'HOST': '0.0.0.0',
-  'PORT': 8080,
-  'wsHOST': '0.0.0.0',
-  'wsPORT': 2999,
-  'pathToYss': '/home/yoyo/Apps/oiyshTerminal/ySS_calibration',
-  'pathsToSites': pathsToSites,
-  "pathsToSitesPackages": pathToSitesPackages,
-  //'wsInjection': false,
-  'wsInjection': true,
-  
-  'sitesInjection': true,
-  'ws': undefined,
-  'wsPinger': false
-};
-
-if( config0.https == true ){
-  config0['yssWSUrl'] = `wss://${hostPublicIp}:${config0.PORT}/fooWSS`;
-} else {
-  // old ws way
-  config0['yssWSUrl'] = `ws://${hostPublicIp}:${config0.wsPORT}`;
-  
-  // noService ws as websocket as plugin in vite
-  //config0['yssWSUrl'] = `ws://${hostPublicIp}:${config0.PORT}/fooWSS`;
-
-}
-
-
-cl(["\n\n---------------------------\nHello - As Dev process name ["+instanceTitle+"]\n",
-  " - dirname: ",__dirname,
-  "\n -------------------------------------------\n\n"
-]);
-
-let sc0 = new serverContainerVite(0,config0 );
-sc0.initServers();
-sc0.startServer();
-
-
-/*
-config1 = JSON.parse(JSON.stringify(config0));;
-config1.name = 'small';
-config1.HOST = 'localhost';
-config1.PORT = 8080;
-config1.wsHOST = 'localhost';
-config1.wsPORT = 2999;
-config1.pathToYss = path.join( nyss.telMeYourHome(`serverHttp ${config1.HOST}:${config1.PORT}`),"yss" );
-config1.pathsToSites = [ path.join( config1.pathToYss, 'sites' ) ]; // add more locations
-config1.yssWSUrl = `ws://localhost:2999/`;
-let sc1 = new serCon.serverContainer( 1, config1 );
-
-sc1.initServers();
-sc1.startServer();
-cl(`Open web browser at: http://${config1.HOST}:${config1.PORT}`);
-*/
-
-cl("Done --- end");
+export { Viteyss }
